f87b85e0d1763aa8afcd7ed9363245e150622122
jean-silva365 <jeanfrancisco2018@outlook.com>
2024-12-05 10:02:23 -0300

    Primeiro

---
+++ Back-End/src/test/java/bcc/anelex/Pet/Shop/ANELEX/PetShopAnelexApplicationTests.java
@@ -0,0 +1,13 @@
+package bcc.anelex.Pet.Shop.ANELEX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopAnelexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
