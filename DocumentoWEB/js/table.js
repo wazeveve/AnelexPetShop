4605565f12150d40f6d7b4c18193faff5d9babb4
Gabriel Venâncio de Carvalho <gabrielvenancio1123@gmail.com>
2024-12-05 09:12:37 -0300

    Primeiro commit (APENAS FRONT)

---
+++ DocumentoWEB/js/table.js
@@ -0,0 +1,46 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    var ctx = document.getElementById('myPieChart').getContext('2d');
+    var myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Puro Fuço', 'Casa do Pet', 'PetShop Scooby-Doo', 'PetShop Paraná', 'Super Dog'],
+            datasets: [{
+                label: 'My Dataset',
+                data: [537, 895, 1351, 650, 851],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                tooltip: {
+                    callbacks: {
+                        label: function(tooltipItem) {
+                            return tooltipItem.label + ': ' + tooltipItem.raw;
+                        }
+                    }
+                }
+            }
+        }
+    });
+});
