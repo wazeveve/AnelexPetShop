b22c3d2e6d62c1927c3c23b845741c6761a78011
jean-silva365 <jeanfrancisco2018@outlook.com>
2024-12-10 10:10:16 -0300

    Criação do Gerente

---
+++ Back-End/src/main/java/bcc/anelex/Pet/Shop/ANELEX/model/repositories/GerenteRepository.java
@@ -0,0 +1,7 @@
+package bcc.anelex.Pet.Shop.ANELEX.model.repositories;
+
+import bcc.anelex.Pet.Shop.ANELEX.model.entities.Gerente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GerenteRepository extends JpaRepository<Gerente, Long> {
+}
