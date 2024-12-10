cb89fe967786d3ace2e9b044719c4b78890be424
jean-silva365 <jeanfrancisco2018@outlook.com>
2024-12-10 10:46:32 -0300

    Produtos

--- Back-End/src/main/java/bcc/anelex/Pet/Shop/ANELEX/model/repositories/ProdutoRepository.java
+++ Back-End/src/main/java/bcc/anelex/Pet/Shop/ANELEX/model/repositories/ProdutoRepository.java
@@ -1,9 +1,10 @@
 package bcc.anelex.Pet.Shop.ANELEX.model.repositories;
 
-import bcc.anelex.Pet.Shop.ANELEX.model.entities.Gerente;
+import bcc.anelex.Pet.Shop.ANELEX.model.entities.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GerenteRepository extends JpaRepository<Gerente, Long> {
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
 }
