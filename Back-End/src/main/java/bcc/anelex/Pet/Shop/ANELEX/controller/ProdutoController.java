149c1ab5a139d6dfd49f04876ee48fa71f325ab3
Max Mohamed Freitas <maxmohamedf@hotmail.com>
2024-12-18 09:27:45 -0300

    Service para Entidade Produto

--- Back-End/src/main/java/bcc/anelex/Pet/Shop/ANELEX/controller/ProdutoController.java
+++ Back-End/src/main/java/bcc/anelex/Pet/Shop/ANELEX/controller/ProdutoController.java
@@ -1,7 +1,9 @@
 package bcc.anelex.Pet.Shop.ANELEX.controller;
 
 import bcc.anelex.Pet.Shop.ANELEX.model.entities.Produto;
+import bcc.anelex.Pet.Shop.ANELEX.model.exceptions.ProdutoNotFoundException;
 import bcc.anelex.Pet.Shop.ANELEX.model.repositories.ProdutoRepository;
+import bcc.anelex.Pet.Shop.ANELEX.model.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,44 +16,36 @@ import java.util.Optional;
 @RequestMapping("produto")
 public class ProdutoController {
     @Autowired
-    private ProdutoRepository produtoRepository;
+    private ProdutoService produtoService;
 
     @GetMapping
     public ResponseEntity<List<Produto>> pegaProdutos(){
-        return ResponseEntity.status(HttpStatus.OK).body(this.produtoRepository.findAll());
+        return ResponseEntity.status(HttpStatus.OK).body(this.produtoService.read());
     }
 
     @PostMapping
     public ResponseEntity<Produto> criaProduto(@RequestBody Produto produto){
-        this.produtoRepository.save(produto);
+        this.produtoService.create(produto);
         return ResponseEntity.status(HttpStatus.CREATED).body(produto);
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Produto> atualizaProduto(@PathVariable Long id, @RequestBody Produto produto){
-        Optional<Produto> optional = this.produtoRepository.findById(id);
-        if(optional.isPresent()){
-            Produto produtoN = (Produto) optional.get();
-            produtoN.setName(produto.getName());
-            produtoN.setDescription(produto.getDescription());
-            produtoN.setPath(produto.getPath());
-            produtoN.setValue(produto.getValue());
-            this.produtoRepository.save(produtoN);
-            return ResponseEntity.status(HttpStatus.CREATED).body(produtoN);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new Produto());
+    public ResponseEntity atualizaProduto(@PathVariable Long id, @RequestBody Produto produto){
+        try{
+            Produto produtoOriginal = this.produtoService.update(id, produto);
+            return new ResponseEntity(produtoOriginal, HttpStatus.OK);
+        } catch (ProdutoNotFoundException pnfe) {
+            return new ResponseEntity(pnfe.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<Produto> deletaProduto(@PathVariable Long id){
-        Optional<Produto> optional = this.produtoRepository.findById(id);
-        if(optional.isPresent()){
-            Produto produtoD = (Produto) optional.get();
-            this.produtoRepository.delete(produtoD);
-            return ResponseEntity.status(HttpStatus.OK).body(produtoD);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new Produto());
+        try{
+            this.produtoService.delete(id);
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } catch (ProdutoNotFoundException pnfe){
+            return new ResponseEntity(pnfe.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 }
