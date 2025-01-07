ae6331f7d3d2a1d277282ab06bd5d7de345d394e
Max Mohamed <maxmohamedf@hotmail.com>
2025-01-07 18:33:36 -0300

    Implementando Relatorio Consultas Controller

---
+++ Back-End/src/main/java/bcc/anelex/Anelex/controller/RelatorioConsultasController.java
@@ -0,0 +1,43 @@
+package bcc.anelex.Anelex.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("relatorioConsultas")
+public class RelatorioConsultasController {
+    @Autowired
+    private RelatorioConsultasService relatorioConsultasService;
+
+    @GetMapping
+    public ResponseEntity<List<RelatorioConsultas>> pegaRelatorios(){
+        return ResponseEntity.status(HttpStatus.OK).body(this.relatorioConsultasService.read());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<RelatorioVendas> pegaRelatorio(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(this.relatorioConsultasService.read(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<RelatorioVendas> salvaRelatorios(@RequestBody RelatorioConsultas relatorioConsultas){
+        this.relatorioConsultasService.create(relatorioConsultas);
+        return ResponseEntity.status(HttpStatus.CREATED).body(relatorioConsultas);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity atualizaRelatorio(@PathVariable Long id, @RequestBody RelatorioConsultas relatorioConsultas){
+        RelatorioConsultas relatorioConsultasOriginal = this.relatorioConsultasService.update(id, relatorioConsultas);
+        return new ResponseEntity(relatorioConsultasOriginal, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<RelatorioConsultas> deletaRelatorio(@PathVariable Long id){
+        this.relatorioConsultasService.delete(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
