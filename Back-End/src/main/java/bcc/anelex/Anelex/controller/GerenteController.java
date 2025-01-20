8ca903ead5e619529be5596b7cc8fc26136fef1f
OtavioHenriquedev8bit <otaviohenrique1098@gmail.com>
2025-01-20 08:43:49 -0300

    alterou repositorio do gerente, inclusao do gerente no sistema de segurança

--- Back-End/src/main/java/bcc/anelex/Anelex/controller/GerenteController.java
+++ Back-End/src/main/java/bcc/anelex/Anelex/controller/GerenteController.java
@@ -1,7 +1,12 @@
 package bcc.anelex.Anelex.controller;
 
+import bcc.anelex.Anelex.dto.LoginRequest;
+import bcc.anelex.Anelex.dto.LoginResponse;
+import bcc.anelex.Anelex.model.entities.Cliente;
 import bcc.anelex.Anelex.model.entities.Gerente;
+import bcc.anelex.Anelex.model.repositories.GerenteRepository;
 import bcc.anelex.Anelex.model.services.GerenteService;
+import bcc.anelex.Anelex.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +18,12 @@ import java.util.List;
 public class GerenteController {
     @Autowired
     private GerenteService gerenteService;
+
+    @Autowired
+    private GerenteRepository gerenteRepository;
+
+    @Autowired
+    private TokenService tokenService;
 
     @GetMapping
     public ResponseEntity<List<Gerente>> pegarGerentes() {
@@ -44,5 +55,15 @@ public class GerenteController {
         this.gerenteService.delete(id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
 
+    }
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> loginClient(@RequestBody LoginRequest loginRequest){
+        Cliente cliente = this.gerenteRepository.findByEmail(loginRequest.email()).orElseThrow(() -> new RuntimeException("Cliente não encontrado!"));
+
+        if(/*passwordEncoder.matches(cliente.getPassword(), loginRequest.password())*/cliente.getPassword().equals(loginRequest.password())){
+            String token = this.tokenService.generateTokenClient(cliente);
+            return ResponseEntity.ok(new LoginResponse(cliente.getUsername(), token));
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
--- Back-End/src/main/java/bcc/anelex/Anelex/model/repositories/GerenteRepository.java
+++ Back-End/src/main/java/bcc/anelex/Anelex/model/repositories/GerenteRepository.java
@@ -1,10 +1,14 @@
 package bcc.anelex.Anelex.model.repositories;
 
 
+import bcc.anelex.Anelex.model.entities.Cliente;
 import bcc.anelex.Anelex.model.entities.Gerente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GerenteRepository extends JpaRepository<Gerente, Long> {
+    Optional<Gerente> findByEmail(String email);
 }
