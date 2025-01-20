8a9aa0b7396d3fc249e7026df33af48497ea65d5
OtavioHenriquedev8bit <otaviohenrique1098@gmail.com>
2025-01-20 15:22:57 -0300

    Criando metodo Test para realização do teste unitário

--- Back-End/src/test/java/bcc/anelex/Anelex/AnelexApplicationTests.java
+++ Back-End/src/test/java/bcc/anelex/Anelex/AnelexApplicationTests.java
@@ -1,5 +1,7 @@
 package bcc.anelex.Anelex;
 
+import bcc.anelex.Anelex.controller.ClienteController;
+import bcc.anelex.Anelex.model.entities.Cliente;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +11,14 @@ class AnelexApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	@Test
+	public void Test(){
+		new ClienteController();
+		Cliente cliente = new Cliente();
+		cliente.setEmail();
+		cliente.setName();
+		cliente.setPets();
+		cliente.setPassword();
+		cliente.setUsername();
+	}
 }
